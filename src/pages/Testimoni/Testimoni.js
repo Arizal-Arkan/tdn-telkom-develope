a7f9dfedb5051ef26d890b925727a3e765a187a2
Arizal-Arkan <arkanotaku@gmail.com>
2021-05-24 11:28:49 +0700

    update

--- src/pages/Testimoni/Testimoni.js
+++ src/pages/Testimoni/Testimoni.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import Webp from '../../components/elements/Webp';
 import styles from './styles.scoped.css';
-import { IMAGES } from '../../configs';
 import DocumentBackground from './../../utils/DocumentBackground';
 import { useMediaQuery } from 'react-responsive';
 
@@ -15,28 +14,28 @@ export default function Testimoni() {
   }
   const listTestimoni = [
     {
-      img: IMAGES.irman,
+      img: 'irman',
       name: 'Irman Juliansyah',
       role: 'Backend Developer',
       testi:
         'DEV itu berasa rumah, orang-orangnya asik, tapi tetap up to date dengan perkembangan teknologi karena stack teknologi yang digunakan juga high.',
     },
     {
-      img: IMAGES.hamdan,
+      img: 'hamdan',
       name: 'Hamdan Prakoso',
       role: 'Frontend Developer',
       testi:
         'For me, its been more than a couple of years working with DEV. DEV provides me with such a conducive and employee-friendly work environment. DEV encourages us to have a balance between work, play, and pray. It also provides a platform to enhance our knowledge and skills via sharing sessions with peers and the leader. I feel very gratified and glad to be associated with DEV.',
     },
     {
-      img: IMAGES.rizkhi,
+      img: 'rizkhi',
       name: 'M. Rizky Pratama',
       role: 'Quality Assurance',
       testi:
         'Telkom DEV memberikan pengalaman dan pembelajaran yang luar biasa. Selain mengerjakan pekerjaan, di sini kita dapat berkembang. Senior di DEV bisa membimbing dan mengarahkan  kita untuk lebih berkembang. Anak-anak DEV juga asik. Serius saat kerja, santai di luar kerja.',
     },
     {
-      img: IMAGES.osi,
+      img: 'osi',
       name: 'Osi Rahmadita',
       role: 'Quality Assurance',
       testi:
@@ -75,7 +74,7 @@ export default function Testimoni() {
           {listTestimoni.map((data, index) => (
             <div className={styles['card-testimoni']} key={index}>
               <div>
-                <img alt="" src={data.img} />
+                <Webp alt="" classImg={styles['img-user']} name={data.img} />
                 <div>
                   <p>{data.name}</p>
                   <p>{data.role}</p>
